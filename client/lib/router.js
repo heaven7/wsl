fa65b28b5bfdb4df7b7620fec3daf8d67a3362e1
Christian Grlica <christian@heavenseven.net>
2015-07-07 19:29:38 +0200

    signup

--- client/lib/router.js
+++ client/lib/router.js
@@ -1,5 +1,7 @@
 Router.map(function() {
     this.route('signup');
+    this.route('login');
+
 
     this.route('home', {
         path: '/'
--- client/views/app-signup.js
+++ client/views/app-signup.js
@@ -6,3 +6,16 @@ Template.signup.helpers({
         return Schema.User;
     }
 });
+Template.signup.events({
+    'submit form': function (event, template) {
+        event.preventDefault();
+        user = new Object
+        user.username = template.$('[name=username]').val();
+        user.email = template.$('[name=email]').val();
+        user.password = template.$('[name=password]').val();
+        Meteor.call('createWSLUser', user, function (error) {
+            if(error)
+                console.log('Error: ' + error.message)
+        })
+    }
+})
