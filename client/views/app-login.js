cad6393179c012b9e3b0c20f7db97d16b86b47b1
Christian Grlica <christian@heavenseven.net>
2015-07-18 17:40:11 +0200

    login & signup with sAlert

--- client/views/app-login.js
+++ client/views/app-login.js
@@ -2,16 +2,32 @@ Template.login.events({
     'submit form#login': function (event, template) {
         event.preventDefault();
 
-        var email = template.$('[name=email]').val();
-        var password = template.$('[name=password]').val();
+        var email = template.$('[name=email]').val()
+        var password = template.$('[name=password]').val()
 
-        Meteor.loginWithPassword(email, password, function(error) {
-            if (error) {
-                sAlert.error(error.message);
-                return false;
+        if(email.length > 0 && password.length > 0) {
+            if(!email) {
+                sAlert.error('Please provide a valid email-adress.')<
+                template.$('[name=email]').focus()
+                return
             }
-            sAlert.success('Successfully logged in');
-            Router.go('home');
-        });
+            if(!password) {
+                sAlert.error('Please fill out password field.')
+                template.$('[name=password]').focus()
+                return
+            }
+            Meteor.loginWithPassword(email, password, function (error) {
+                if (error) {
+                    sAlert.error(error.message);
+                    return
+                }
+                sAlert.success('Successfully logged in');
+                Router.go('home');
+            })
+        } else {
+            sAlert.error('Please fill out all necessary fields.')
+            template.$('[name=email]').focus()
+            return
+        }
     }
 })
--- client/views/app-signup.js
+++ client/views/app-signup.js
@@ -13,14 +13,38 @@ Template.signup.events({
         user.username = template.$('[name=username]').val();
         user.email = template.$('[name=email]').val();
         user.password = template.$('[name=password]').val();
+        user.password_confirmation = template.$('[name=password_confirmation]').val();
 
-        if(!user.username)
-            sAlert.error('Please provide a username.')
+        if(!user.username) {
+            sAlert.error('Please fill out username field.')
+            template.$('[name=username]').focus()
             return
-        
+        }
+        if(!user.email) {
+            sAlert.error('Please provide a valid email-adress.')
+            template.$('[name=email]').focus()
+            return
+        }
+        if(!user.password) {
+            sAlert.error('Please fill out password field.')
+            template.$('[name=password]').focus()
+            return
+        }
+        if(!user.password_confirmation) {
+            sAlert.error('Please confirm your password.')
+            template.$('[name=password_confirmation]').focus()
+            return
+        }
+
+        if(user.password != user.password_confirmation) {
+            sAlert.error('Your password does not match.')
+            template.$('[name=password]').focus()
+            return
+        }
+
         Meteor.call('createWSLUser', user, function (error) {
             if(error)
-                Notifications.throw(error.message, 'error')
+                sAlert.error(error.message)
 
             Router.go('home');
         })
